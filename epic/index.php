09704adef35c94464dcc6875f4036cb9dfba1814
JRyanHenson <hensojr@gmail.com>
2017-07-13 22:39:08 -0600

    Created new project for data-design assignment

---
+++ epic/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design</title>
+	</head>
+	<body>
+		<p>Persona -</p><p>Use Case -</p><p>Interaction Flow -</p> <p>Conceptual Model -</p>
+	</body>
+</html>
