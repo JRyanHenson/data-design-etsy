39c5c835badc5593b6623c0937aebd11aa8fb662
JRyanHenson <hensojr@gmail.com>
2017-07-14 10:08:36 -0600

    Updated the name of the site and began adding data to the Persona section and Conceptual Model section.

--- epic/index.php
+++ epic/index.php
@@ -2,9 +2,29 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Data Design</title>
+		<title>Crapsy Data Design</title>
 	</head>
 	<body>
-		<p>Persona -</p><p>Use Case -</p><p>Interaction Flow -</p> <p>Conceptual Model -</p>
+		<h2>Persona</h2>
+		<p><strong>Name:</strong>Sally Seller</p>
+		<p><strong>Age:</strong>32</p>
+		<p><strong>Technology:</strong>Windows moderate user. Sally uses Dell Inspiron 3000 series, and an iPhone 6 with a limited data plan. </p>
+		<p><strong>Attitudes and Behaviors:</strong>Sally is intelligent, creative, and meticulous but not a power computer user. She can learn quickly but can be overwhelmed when tasks become too technical. She has a full-time job and one child so her free time is limited</p>
+		<p><strong>Frustrations and Needs:</strong>While by day Sally is a senior Geologist with the U.S. Geological Survey, her true passion is making Elvis paper mache dolls. She needs a user friendly site where she can easily sell her art. Sally is a good fit for Crapsy because she needs an easy to use platform for displaying and selling her art.</p>
+		<p><strong>Goals:</strong>Sally's goal is be a full time artisan and eventually move from paper mache Elvis's to paintings of Marilyn Monroe on velvet backgrounds.</p>
+		<p><strong>User Story:</strong>As a user, Sally needs to easily set up a Crapsy account, upload a description and picture of my product and sell it quickly</p>
+
+		<h2>Use Case</h2>
+		<p>...</p>
+
+		<h2>Interaction Flow</h2>
+		<p>...</p>
+
+		<h2>Conceptual Model</h2>
+		<ol>Relationships:</ol>
+			<li>One user has one profile</li>
+			<li>One user has many products</li>
+			<li>One product has one description</li>
+			<li>One product has many reviews</li>
 	</body>
 </html>
